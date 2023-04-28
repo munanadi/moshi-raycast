7cdeab55ac05b9255c41f4fb9491ae023532b7a3
nana <ajibaa@gmail.com>
2023-04-28 20:59:13 +0530

    chore: data

---
+++ raycast-env.d.ts
@@ -0,0 +1,38 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `market-data` command */
+  export type MarketData = ExtensionPreferences & {}
+  /** Preferences accessible in the `coin-gecko-tokens` command */
+  export type CoinGeckoTokens = ExtensionPreferences & {}
+  /** Preferences accessible in the `tokens-list` command */
+  export type TokensList = ExtensionPreferences & {}
+  /** Preferences accessible in the `ticker` command */
+  export type Ticker = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `market-data` command */
+  export type MarketData = {}
+  /** Arguments passed to the `coin-gecko-tokens` command */
+  export type CoinGeckoTokens = {}
+  /** Arguments passed to the `tokens-list` command */
+  export type TokensList = {}
+  /** Arguments passed to the `ticker` command */
+  export type Ticker = {
+  /** Symbol/Base_Target */
+  "base": string
+}
+}
