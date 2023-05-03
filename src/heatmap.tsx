14a8b5bfe662ceb8397d4610842ee230b273b8ad
nana <ajibaa@gmail.com>
2023-05-04 01:04:49 +0530

    heatmap chart fetched

--- src/heatmap.tsx
+++ src/heatmap.tsx
@@ -1,16 +1,36 @@
-import { Detail } from "@raycast/api";
+import { Detail, Toast, showToast } from "@raycast/api";
+import { useFetch } from "@raycast/utils";
+import { useEffect } from "react";
 
 const Command = () => {
-  /* 
- 1. Load the image from the API
- 2. Show loading spinner
- 3. Display the image
- */
+  const {
+    data: fetchedData,
+    isLoading,
+    error,
+  } = useFetch<any>(
+    "https://elegant-phoenix-680f0e.netlify.app/.netlify/functions/renderHeatmap"
+  );
+
+  const data = JSON.parse(fetchedData);
+
+  useEffect(() => {
+    (async () => {
+      if (error || !data?.file_url) {
+        await showToast({
+          style: Toast.Style.Failure,
+          title: "Fetching data went wrong",
+        });
+      }
+    })();
+  }, [error, data]);
+
+  const markdown =
+    isLoading || !data?.file_url
+      ? `# Fetching data...`
+      : `<img alt="heatmap" height="345" src="${data.file_url}">`;
 
   return (
-    <Detail
-      markdown={`<img alt="heatmap" height="345" src="/Users/aadhi/Desktop/chart.jpg">`}
-    />
+    <Detail isLoading={isLoading} markdown={markdown} />
   );
 };
 
